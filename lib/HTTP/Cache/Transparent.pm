d75599704060858606b754cbf6f30a1a94623d02
Darren Embry <dse@webonastick.com>
2014-09-26 17:46:40 -0400

    remove honey badger superpowers message.

--- lib/HTTP/Cache/Transparent.pm
+++ lib/HTTP/Cache/Transparent.pm
@@ -625,11 +625,4 @@ at your option, any later version of Perl 5 you may have available.
 
 =cut
 
-BEGIN {
-  my %me = (dse => 1, dembry => 1);
-  if ($me{$ENV{USER}} || $me{$ENV{LOGNAME}}) {
-    warn("This version of HTTP::Cache::Transparent has Honey Badger Superpowers!\n");
-  }
-}
-
 1;
